2c80c2762c7ab73cbe8a1b994f0215a96377727e
raydeng2007 <r6deng@uwaterloo.ca>
2022-04-13 14:37:19 -0400

    add solidity support

--- src/components/Code.js
+++ src/components/Code.js
@@ -4,7 +4,7 @@ import lightTheme from 'prism-react-renderer/themes/vsLight';
 import React from 'react';
 import styled from 'styled-components';
 import { copyToClipboard } from '../utils/copy-to-clipboard';
-
+require("prismjs/components/prism-solidity");
 export const Pre = styled.pre`
   text-align: left;
   margin: 1rem 0;
